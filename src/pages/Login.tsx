2a648d7cf94bbd390880069c836cfeb1a04d5298
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-01-11 22:14:01 +0600

    set user in local stroge use redux persist

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,26 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { setUser } from "../redux/features/auth/authSlice";
+import { useAppDispatch } from "../redux/hooks";
+import { verifyToken } from "../utils/verifyToken";
 
 const Login = () => {
     const {register, handleSubmit} = useForm()
+    const [login] = useLoginMutation()
+    const dispatch = useAppDispatch()
     const onSubmit = async (data) => {
-        console.log(data);
+        const userInfo = {
+            id: data.id,
+            password: data.password,
+          };
+
+        const res = await login(userInfo).unwrap()
+        const user = verifyToken(res.data.accessToken)
+        console.log(user);
+        
+        dispatch(setUser({ user: user, token: res.data.accessToken }))
+        
     }
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
