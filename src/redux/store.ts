47569f62219ef5523482b6c8d68ca061ef58b177
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-01-11 21:09:05 +0600

    base api create wiht redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { baseApi } from "./Api/baseApi";
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        [baseApi.reducerPath] : baseApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
