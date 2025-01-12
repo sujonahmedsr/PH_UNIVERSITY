e1a63199436b372922f6fd003ae48da77e7778e4
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-01-12 21:01:06 +0600

    handle refresh token

--- src/redux/Api/baseApi.ts
+++ src/redux/Api/baseApi.ts
@@ -1,20 +1,48 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { logOut, setUser } from "../features/auth/authSlice";
 
-const baseApiQuery = fetchBaseQuery({
+const baseQuery = fetchBaseQuery({
     baseUrl: 'http://localhost:5000/api/v1',
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
         const toekn = (getState() as RootState).auth.token
-        if(toekn){
+        if (toekn) {
             headers.set("authorization", `${toekn}`)
         }
         return headers
     }
 })
 
+const baseQueryWithRefreshToken = async (args, api, extraOption) => {
+    let result = await baseQuery(args, api, extraOption)
+
+    if (result?.error?.status === 401) {
+        console.log('Sending refresh token');
+        const res = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
+            method: "POST",
+            credentials: "include"
+        })
+        const data = await res.json()
+
+        if (data?.data?.accessToken) {
+            const user = (api.getState() as RootState).auth.user;
+            api.dispatch(
+                setUser({
+                    user,
+                    token: data.data.accessToken,
+                })
+            );
+
+        }
+        result = await baseQuery(args, api, extraOption);
+    }
+
+    return result
+}
+
 export const baseApi = createApi({
     reducerPath: "baseApi",
-    baseQuery: baseApiQuery,
+    baseQuery: baseQueryWithRefreshToken,
     endpoints: () => ({})
 })
