31fbeb0682b9952eb447360624142419ef43a1f4
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-01-12 20:00:40 +0600

    prepareheaders setup in baseApi

--- src/pages/Admin/academicManagement/AcademicSemester.tsx
+++ src/pages/Admin/academicManagement/AcademicSemester.tsx
@@ -1,5 +1,9 @@
+import { useGetAllSemestersQuery } from "../../../redux/features/academicSemester/academicSemesterApi";
 
 const AcademicSemester = () => {
+    const { data } = useGetAllSemestersQuery(undefined);
+
+    console.log(data);
     return (
         <div>
             <h1> This is AcademicSemester component </h1>
--- src/redux/Api/baseApi.ts
+++ src/redux/Api/baseApi.ts
@@ -1,8 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+
+const baseApiQuery = fetchBaseQuery({
+    baseUrl: 'http://localhost:5000/api/v1',
+    credentials: "include",
+    prepareHeaders: (headers, { getState }) => {
+        const toekn = (getState() as RootState).auth.token
+        if(toekn){
+            headers.set("authorization", `${toekn}`)
+        }
+        return headers
+    }
+})
 
 export const baseApi = createApi({
     reducerPath: "baseApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:5000/api/v1', credentials: "include"}),
+    baseQuery: baseApiQuery,
     endpoints: () => ({})
 })
