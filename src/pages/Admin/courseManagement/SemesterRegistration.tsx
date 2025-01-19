2fc180fa45f6c3b3eb2464af737ac333b0478159
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-01-20 00:09:32 +0600

    semesterRegis

--- src/pages/Admin/courseManagement/SemesterRegistration.tsx
+++ src/pages/Admin/courseManagement/SemesterRegistration.tsx
@@ -1,9 +1,38 @@
+import { Button, Col, Flex } from "antd";
+import PHForm from "../../../components/form/PHForm";
+import PHDatePicker from "../../../components/form/PHDatePicker";
+import PHInput from "../../../components/form/PHInput";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import PHSelect from "../../../components/form/PHSelect";
 
 const SemesterRegistration = () => {
+    const onsubmit: SubmitHandler<FieldValues> = (data) => {
+        console.log(data);
+    }
     return (
-        <div>
-            SemesterRegistration.tsx
-        </div>
+        <Flex justify="center">
+            <Col span={7}>
+                <PHForm onSubmit={onsubmit}>
+                    <PHSelect
+                        label="Academic Semester"
+                        name="academicSemester"
+                        options={academicSemesterOptions}
+                    />
+
+                    <PHSelect
+                        name="status"
+                        label="Status"
+                        options={semesterStatusOptions}
+                    />
+                    <PHDatePicker name="startDate" label="Start Date" />
+                    <PHDatePicker name="endDate" label="End Date" />
+                    <PHInput type="text" name="minCredit" label="Min Credit" />
+                    <PHInput type="text" name="maxCredit" label="Max Credit" />
+
+                    <Button htmlType="submit">Submit</Button>
+                </PHForm>
+            </Col>
+        </Flex>
     );
 };
 
